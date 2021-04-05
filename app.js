d89f60bae6693b2081c473bfd6ddf304ec796be0
mauriciomena <mauricio.mena.arg@gmail.com>
2021-04-05 19:18:22 -0300

    Mena- Adaptacines para trabajar con Heroku

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT;
 const path = require('path');
 
 app.use(express.static('public'));
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
@@ -17,6 +16,6 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
-app.listen(port, () => {
+app.listen(port || 3000, () => {
     console.log('MercadoLiebre app listening on port '+ port);
 });
